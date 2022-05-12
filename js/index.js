b0e85ca9c6dc7f77ff78aaba7fa50289843fb6d0
WenyiLULU <luwenyi1991@gmail.com>
2022-05-12 22:50:31 +0200

    score and game over done

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const startScreen = document.querySelector('.game-intro');
 const gameBord = document.querySelector('#game-board');
 const canvas = document.querySelector('#canvas');
 
+
 // canvas variables settings
 const ctx = canvas.getContext("2d");
 const imgRaod = new Image();
@@ -21,10 +22,16 @@ let carGoLeft = false;
 let carGoRight = false;
 
 // blocks variables settings
-const blockHeight = 10;
-const blockWidthMax = canvas.width - carWidth * 5;
+const blockHeight = 20;
+const blockWidthMax = canvas.width - carWidth * 6;
 const blockSpeed = 1;
-let blockY = 0;
+let spaceBlock = -220;
+
+// score and finish game
+let score = 0;
+let gameOver = false;
+let animationFrameID 
+
 
 
 // ---- set game ground -------
@@ -56,62 +63,92 @@ function getRandomX(){
 function getRandomBlockW(){
   return Math.floor(Math.random() * blockWidthMax) + 200;
 }
-
-
 const blocks = [
   {
     x: getRandomX(),
-    y: blockY,
+    y: 0,
     w: getRandomBlockW(),
     h: blockHeight
   },
   {
     x: getRandomX(),
-    y: blockY-200,
+    y: spaceBlock,
     w: getRandomBlockW(),
     h: blockHeight
   },
   {
     x: getRandomX(),
-    y: blockY-400,
+    y: spaceBlock * 2,
     w: getRandomBlockW(),
     h: blockHeight
   },
 ]
-
 function drawBlock(x, y, w, h) {
   ctx.beginPath();
   ctx.fillStyle = "tomato";
   ctx.fillRect(x, y, w, h);
   ctx.closePath(); 
 }
-
-function moveBlock(){
-  
+function moveBlock(){  
   blocks.forEach((block) => {
-    block.y += blockSpeed;
+   
+    if (block.y + block.h === carY){
+      if (carX <= block.x + block.w && carX >= block.x){
+        gameOver = true;
+      } else {
+        score += 1;
+      }
+    }
+    
     if(block.y > canvas.height){
       block.y = 0;
       block.x = getRandomX();
       block.w = getRandomBlockW();
     }
+    block.y += blockSpeed;
     drawBlock(block.x, block.y, block.w, block.h);
-    console.log(blocks)
   })
 }
 
+// ---- draw score -----
+function drawScore() {
+  ctx.beginPath();
+  ctx.font = "30px sans-serif";
+  ctx.fillStyle = "white";
+  ctx.fillText(`Score : ${score}`, 80, 50);
+  ctx.closePath();
+}
 
+// ---- draw game over board ----
+function drawGameOverBoard() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, canvas.height/2, canvas.width, canvas.height/2);
+  ctx.beginPath();
+  ctx.font = "40px sans-serif";
+  ctx.fillStyle = "red";
+  ctx.fillText(`Game Over`, 140, canvas.height/10*7);
+  ctx.fillStyle = "white";
+  ctx.fillText(`Your score is`, 130, canvas.height/10*8);
+  ctx.fillText(`${score}`, 230, canvas.height/10*9);
+  ctx.closePath();
+}
 
+// ---- animate page -----
 function animate(){
   ctx.clearRect(0, 0, canvas.width, canvas.height)
   ctx.drawImage(imgRaod, 0, 0, canvas.width, canvas.height)
   drawCar()
   moveBlock()
-
-  requestAnimationFrame(animate)
-
+  drawScore()
+  if (gameOver) {
+    cancelAnimationFrame(animationFrameID);
+    drawGameOverBoard();
+  } else {
+    animationFrameID = requestAnimationFrame(animate);
+  }
 }
 
+// ---- initialize game -----
 function startGame() {
   gameBord.style.display = 'block';
   setGrameBord()
